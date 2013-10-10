c2c18c240b64e7efd713936a8513487d85cff5ef
Mauricio Muñoz <gmauricio.munoz@gmail.com>
2013-10-09 23:57:55 -0500

    set travis to use mysql for testing

--- test_settings.py
+++ test_settings.py
@@ -3,8 +3,8 @@ from settings import *
 #Disable South to avoid migrations when creating test database
 INSTALLED_APPS = [app for app in INSTALLED_APPS if app != 'south']
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3'
-    }
-}
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3'
+#    }
+#}
