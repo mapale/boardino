e13325f101cab28d89706c04cc1b667d54174ab8
Mauricio Muñoz <gmauricio.munoz@gmail.com>
2013-10-09 22:40:58 -0500

    sqlite DATABASE settings for testing

--- test_settings.py
+++ test_settings.py
@@ -2,3 +2,9 @@ from settings import *
 
 #Disable South to avoid migrations when creating test database
 INSTALLED_APPS = [app for app in INSTALLED_APPS if app != 'south']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3'
+    }
+}
