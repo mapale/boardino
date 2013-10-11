d703b3238186d28f6dc9b433f864ef3643db937b
velasquez <andres@mapalesoftware.com>
2013-10-10 22:03:42 -0500

    save authenticated users boardinos

--- accounts/models.py
+++ accounts/models.py
@@ -1,8 +1,10 @@
 from django.contrib.auth.models import User
 from django.db import models
 from userena.models import UserenaBaseProfile
+from board.models import Board
 
 
 class UserProfile(UserenaBaseProfile):
-    user = models.OneToOneField(User,
-                                unique=True)
+    user = models.OneToOneField(User, unique=True)
+    boardinos = models.ManyToManyField(Board)
+
