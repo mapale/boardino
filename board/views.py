c8ce09d98032fbafe2a78b1e16b7a769f3deb5a6
Mauricio Muñoz <gmauricio.munoz@gmail.com>
2014-02-26 15:32:36 -0500

    added i18n

--- board/views.py
+++ board/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponseRedirect, HttpResponse, HttpResponseForbidde
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
 from django.views.decorators.csrf import csrf_exempt
+from django.utils.translation import ugettext as _
 from rest_framework.decorators import api_view
 from rest_framework import generics
 from rest_framework.response import Response
@@ -13,31 +14,31 @@ from board.models import Board, PostIt, Line, Text
 from board.serializers import PostitSerializer, LineSerializer, BoardSerializer, TextSerializer
 
 
+
 def home(request):
     visited_boards = []
     if 'visited' in request.session:
         visited_boards = request.session['visited']
-    form = BoardForm()
     return render_to_response('home.html',
-                              {'visited_boards': visited_boards, 'board_form': form},
+                              {'visited_boards': visited_boards, 'board_form': BoardForm()},
                               context_instance=RequestContext(request))
 
 @csrf_exempt
 def subscribe(request):
 
     if "email" not in request.POST or not validateEmail(request.POST["email"]):
-        json_data = json.dumps({"result":"Pleas enter a valid email"})
+        json_data = json.dumps({"result":_("Please enter a valid email")})
         return HttpResponse(json_data, mimetype="application/json")
 
     if request.is_ajax():
         email = request.POST["email"]
         try:
             User.objects.get(email__exact=email)
-            json_data = json.dumps({"result":"The email is already registered"})
+            json_data = json.dumps({"result":"_(The email is already registered)"})
         except User.DoesNotExist:
             user = User.objects.create_user(email, email)
             user.save()
-            json_data = json.dumps({"result":"Subscribed!"})
+            json_data = json.dumps({"result":_("Subscribed!")})
         return HttpResponse(json_data, mimetype="application/json")
     else:
         return HttpResponse(status=400)
@@ -61,16 +62,16 @@ def create_board(request):
             new_board = Board()
             new_board.save()
 
-            welcomePostit = PostIt(text="Welcome! Move me! Edit me! Delete me!",x=120,y=50, board=new_board, width=100,
+            welcomePostit = PostIt(text=_("Welcome! Move me! Edit me! Delete me!"),x=120,y=50, board=new_board, width=100,
                                    height=100)
-            sharePostit = PostIt(text="Share this board and work together in realtime!\n\nhttp://www.boardino.com/"+new_board.hash,
+            sharePostit = PostIt(text=_("Share this board and work together in realtime!")+ "\n\nhttp://www.boardino.com/"+new_board.hash,
                             x=200,
                             y=300,
                             board=new_board,
                             width=220,
                             height=100,
                             back_color='#FF69B4')
-            comeBackPostit = PostIt(text="Come back to check new features!",x=550,y=50, board=new_board,
+            comeBackPostit = PostIt(text=_("Come back to check new features!"),x=550,y=50, board=new_board,
                                     width=150,
                                     height=100,
                                     back_color='#ADFF2F')
